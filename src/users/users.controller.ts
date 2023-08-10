c2f8b2a7e82013af1d8640ced8fa8850a881e295
gboutte <btte.gregory@gmail.com>
2023-08-10 21:02:42 +0200

    user management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 @ApiTags('users')
@@ -11,5 +22,38 @@ export class UsersController {
   @ApiBearerAuth()
   getAll() {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @ApiBearerAuth()
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The uuid of the user',
+  })
+  async get(@Param('id') id: string): Promise<User> {
+    const user = await this.usersService.findOne(id);
+    if (user !== null) {
+      return user;
+    } else {
+      throw new NotFoundException(); // You can get information only on registered user
+    }
+  }
+
+  @Patch(':id')
+  @ApiBearerAuth()
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'The uuid of the user',
+  })
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Post()
+  @ApiBearerAuth()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 }
