5530921528cae73156e9fb9ca826cbe3eaf29fea
gboutte <btte.gregory@gmail.com>
2025-12-07 19:41:14 +0100

    refactor: change bootstrap app

---
+++ client/src/app.config.ts
@@ -0,0 +1,42 @@
+import {ApplicationConfig, importProvidersFrom} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {provideRouter, RouterOutlet} from "@angular/router";
+import {ConfigModule} from "./app/config/config.module";
+import {AuthModule} from "./app/auth/auth.module";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HTTP_INTERCEPTORS, HttpClient} from "@angular/common/http";
+import {AuthGuard} from "./app/auth/auth.guard";
+import {AuthInterceptor} from "./app/auth/interceptor/auth.interceptor";
+import {RetryInterceptor} from "./app/global/interceptors/retry.interceptor";
+import {routes} from "./app.routes";
+import {MultiTranslateHttpLoader} from "./app/global/multi-translate-http-loader";
+
+export function HttpLoaderFactory(http: HttpClient): MultiTranslateHttpLoader {
+  return new MultiTranslateHttpLoader(http);
+}
+
+export const appConfig:ApplicationConfig = {
+  providers: [
+    importProvidersFrom(BrowserModule, RouterOutlet, ConfigModule, AuthModule, TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })),
+
+    provideRouter(routes),
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RetryInterceptor,
+      multi: true,
+    },
+  ]
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,20 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { FormControl, FormGroup } from '@angular/forms';
-import { AppModule, HttpLoaderFactory } from './app/app.module';
-import { AuthGuard } from './app/auth/auth.guard';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { AuthInterceptor } from './app/auth/interceptor/auth.interceptor';
-import { RetryInterceptor } from './app/global/interceptors/retry.interceptor';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { RouterOutlet } from '@angular/router';
-import { AppRoutingModule } from './app/app-routing.module';
-import { ConfigModule } from './app/config/config.module';
-import { AuthModule } from './app/auth/auth.module';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { MultiTranslateHttpLoader } from './app/global/multi-translate-http-loader';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRootComponent } from './app/app-root/app-root.component';
-import { importProvidersFrom } from '@angular/core';
+import {appConfig} from "./app.config";
 
 declare module '@angular/forms' {
   interface FormGroup {
@@ -38,27 +25,5 @@ FormGroup.prototype.updateAllValueAndValidity = (
 
   updateFormGroup(formGroup);
 };
-bootstrapApplication(AppRootComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, RouterOutlet, AppRoutingModule, ConfigModule, AuthModule, TranslateModule.forRoot({
-            defaultLanguage: 'en',
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient],
-            },
-        })),
-        AuthGuard,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true,
-        },
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: RetryInterceptor,
-            multi: true,
-        },
-    ]
-})
+bootstrapApplication(AppRootComponent, appConfig)
   .catch((err) => console.error(err));
